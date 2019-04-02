846f29a999d0e5db32e6fc4da280b0feeb34780a
Neil Hawkins <neilh@cogapp.com>
2019-04-02 19:03:10 +0100

    Extract speech tags and make up ideal titles using those

--- 6/main.py
+++ 6/main.py
@@ -1,3 +1,4 @@
+from collections import defaultdict, Counter
 import json
 import os
 import argparse
@@ -36,6 +37,58 @@ def most_frequent_words(data, year, num, stop, extra_stops):
   return fdist.most_common(num)
 
 
+def get_docs(data, year, num):
+  docs = []
+
+  if '-' in year:
+    start, end = year.split('-')
+  else:
+    start = year
+    end = int(year) + 1
+
+  for year in range(int(start), int(end)):  
+    titles = list(set([title['title'] for title in data if title['year'] == str(year)])) 
+    docs += titles
+
+  return docs
+
+
+def use_spacy(docs):
+  # python main.py -d 2001-2005
+  # prints a list of n most common words from the 5 years period and part-of-speech tag e.g. verb, adj etc
+  # see https://spacy.io/usage/linguistic-features#pos-tagging
+  # for more info
+  import spacy
+  nlp = spacy.load('en')
+
+  pos_counts = defaultdict(Counter)
+  docs = " ".join(docs)
+  doc = nlp(docs)
+  
+  for token in doc:
+    pos_counts[token.pos][token.orth] += 1
+
+  words = []
+  for pos_id, counts in pos_counts.items():
+    pos = doc.vocab.strings[pos_id]
+    for orth_id, count in counts.most_common(2):
+      print(pos_id, pos, count, doc.vocab.strings[orth_id])
+      words.append(doc.vocab.strings[orth_id])
+  
+  print(" ".join(words))
+
+def use_textacy(text):
+  import textacy
+  import textacy.keyterms
+  doc = textacy.Doc(text)
+
+  print(textacy.keyterms.textrank(doc, normalize='lemma', n_keyterms=10))
+  bot = doc.to_bag_of_terms(
+       ngrams=(1, 2, 3), named_entities=True, weighting='count',
+       as_strings=True)
+  print(sorted(bot.items(), key=lambda x: x[1], reverse=True)[:15])
+
+
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
   parser.add_argument('-d', '--year',
@@ -52,5 +105,7 @@ if __name__ == '__main__':
   path, filename = os.path.split(os.path.realpath(__file__))
   with open(os.path.join(path, '../data/all.json')) as fh:
     parsed = json.loads(fh.read())
-    print(most_frequent_words(parsed, year=args.year, num=args.words, stop=args.stop, extra_stops=args.customstop))
-
+    docs = get_docs(parsed, args.year, args.words)
+    # print(most_frequent_words(parsed, year=args.year, num=args.words, stop=args.stop, extra_stops=args.customstop))
+    # use_textacy(text)
+    use_spacy(docs)
